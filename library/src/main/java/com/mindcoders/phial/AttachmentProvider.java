f777d666f3b181b732c1e93040b5b2e1b56e478a
roshakorost <roshak.rostyslav@gmail.com>
2017-10-22 22:17:38 +0300

    renamed log provider to attachment provider

---
+++ library/src/main/java/com/mindcoders/phial/AttachmentProvider.java
@@ -0,0 +1,15 @@
+package com.mindcoders.phial;
+
+import java.io.File;
+
+/**
+ * Created by rost on 10/22/17.
+ */
+
+public interface AttachmentProvider {
+    /**
+     * Provides phial with attachment that will be associated with debug data.
+     * If method returns directory all files from it would be attached to debug data.
+     */
+    File provideAttachement();
+}
