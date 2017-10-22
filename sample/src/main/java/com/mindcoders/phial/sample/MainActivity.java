8f16e09c0b99a944acd7798b2f7a4f48a5c2fa79
roshakorost <roshak.rostyslav@gmail.com>
2017-10-22 18:21:27 +0300

    Created basic project structure

---
+++ sample/src/main/java/com/mindcoders/phial/sample/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mindcoders.phial.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mindcoders.phial.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
