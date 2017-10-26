daaa1a150654b83c624f65028f5ae0ef10f0bae1
roshakorost <roshak.rostyslav@gmail.com>
2017-10-26 18:12:27 +0300

    changed libraries namings

--- phial-overlay/src/main/java/com/mindcoders/phial/internal/share/ShareAdapter.java
+++ phial-overlay/src/main/java/com/mindcoders/phial/internal/share/ShareAdapter.java
@@ -1,7 +1,6 @@
 package com.mindcoders.phial.internal.share;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
