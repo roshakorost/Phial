8b12581cacb5e323ca5408c2cb9186e05d452d80
Andriy Matkovsky <andriymatkovsky@gmail.com>
2017-10-29 12:14:11 +0200

    add doc

--- phial-overlay/src/main/java/com/mindcoders/phial/PageView.java
+++ phial-overlay/src/main/java/com/mindcoders/phial/PageView.java
@@ -2,6 +2,10 @@ package com.mindcoders.phial;
 
 public interface PageView {
 
+    /**
+     * Called when device back button has been pressed
+     * @return true if the event was consumed; false otherwise.
+     */
     boolean onBackPressed();
 
 }
