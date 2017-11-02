9498dba91cf953c9c87ee04e9c434d64e7f02b91
roshakorost <roshak.rostyslav@gmail.com>
2017-11-02 12:03:46 +0200

    finished javadocs for share options

--- phial-overlay/src/main/java/com/mindcoders/phial/ShareContext.java
+++ phial-overlay/src/main/java/com/mindcoders/phial/ShareContext.java
@@ -4,22 +4,59 @@ import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.view.View;
 
-/**
- * Created by rost on 10/27/17.
- */
+import java.io.File;
 
+/**
+ * Used for communication with Phial when share option is selected see {@link Shareable#share(ShareContext, File, String)}
+ * <p>
+ * At the end of share should notify Phial about result of sharing by calling one of
+ * {@link #onSuccess()}
+ * {@link #onFailed(String)}
+ * {@link #onCancel()}
+ */
 public interface ShareContext {
+    /**
+     * @return Android Context in which Phial works
+     */
     Context getAndroidContext();
 
+    /**
+     * Should be called if successfully shared
+     */
     void onSuccess();
 
+    /**
+     * Should be called in case of error during share.
+     *
+     * @param message that will be presented to user
+     */
     void onFailed(String message);
 
+    /**
+     * Should be called in case user cancel share
+     */
     void onCancel();
 
+    /**
+     * Presents custom view that will be shown to user
+     *
+     * @param view view to present
+     */
     void presentView(View view);
 
+    /**
+     * Shows or hides progress bar to user
+     *
+     * @param isVisible true will show progressbar false will hide
+     */
     void setProgressBarVisibility(boolean isVisible);
 
+    /**
+     * Creates view using provided layoutId. Created view will not be presented.
+     * Should {@link #presentView(View)} in order to show to user
+     *
+     * @param layoutId Android layout resource
+     * @return created view
+     */
     View inflate(@LayoutRes int layoutId);
 }
